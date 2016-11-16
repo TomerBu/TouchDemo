c829d92d9a1427bda49dc778318c3cd57ab9edc8
tomerbuzaglo <tomer.bu@gmail.com>
2016-11-16 15:46:31 +0200

    Using SparseArray

--- app/src/main/java/tomerbu/edu/touchdemo/MultiTouchView.java
+++ app/src/main/java/tomerbu/edu/touchdemo/MultiTouchView.java
@@ -6,26 +6,29 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.HashMap;
-
 /**
- * Created by admin on 07/05/2015.
+ * Created by TomerBu on 07/05/2015.
  */
 public class MultiTouchView extends View {
     private static final float RADIUS = 50;
     private Paint mPaint;
-    private HashMap<Integer,PointF> mActivePointers = new HashMap<>();
-    private static  final int[] COLORS = {Color.BLUE,Color.YELLOW, Color.CYAN, Color.GREEN, Color.MAGENTA,Color.RED, Color.GRAY};
+    //private HashMap<Integer, PointF> mActivePointers = new HashMap<>();
+    private SparseArray<PointF> mActivePointers = new SparseArray<>();
+    private static final int[] COLORS = {Color.BLUE, Color.YELLOW, Color.CYAN, Color.GREEN, Color.MAGENTA, Color.RED, Color.GRAY};
+
     //Constructors
     public MultiTouchView(Context context) {
         this(context, null);
     }
+
     public MultiTouchView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
+
     public MultiTouchView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
@@ -38,32 +41,45 @@ public class MultiTouchView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int pointerIndex = event.getActionIndex();
-        int pointerId = event.getPointerId(pointerIndex);
+        //the masked action, returns either ACTION_POINTER_DOWN Or ACTION_POINTER_UP, Or ACTION_MOVE (For all the pointers movement together).
         int actionMasked = event.getActionMasked();
+        //The number of pointers
+        int pointerCount = event.getPointerCount();
+        int pointerIndex = event.getActionIndex();
 
-        switch (actionMasked){
+        //getX(int idx) returns the x coordinate for a specific pointer
+        //getY(int idx) returns the y coordinate for a specific pointer
+
+        //A MotionEvent effectively stores information about each pointer in an array. The index of a pointer is its position within this array.
+
+        //Each pointer also has an ID mapping that stays persistent across touch events to allow tracking an individual pointer across the entire gesture.
+        int pointerId = event.getPointerId(pointerIndex);
+
+
+        switch (actionMasked) {
+
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
                 PointF point = new PointF();
                 float x = event.getX();
                 float y = event.getY();
-                point.set(x,y);
-                mActivePointers.put(pointerId,point);
+                point.set(x, y); //same as: //point.x = x; point.y = y;
+                mActivePointers.put(pointerId, point);
                 break;
 
             case MotionEvent.ACTION_MOVE:
                 for (int i = 0; i < event.getPointerCount(); i++) {
                     int currentPointerId = event.getPointerId(i);
                     PointF pointf = mActivePointers.get(currentPointerId);
-                        if (pointf != null) {
-                            pointf.x = event.getX(i); //The point is updated in the HashMap/Dictionary by reference
-                            pointf.y = event.getY(i);
-                        }
+                    if (pointf != null) {
+                        pointf.x = event.getX(i); //The point is updated in the HashMap/Dictionary by reference
+                        pointf.y = event.getY(i);
+                    }
                 }
                 break;
-            case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
                 mActivePointers.remove(pointerId);
                 break;
         }
@@ -73,11 +89,14 @@ public class MultiTouchView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        for (Integer key : mActivePointers.keySet()) {
+        //for (Integer key : mActivePointers.keySet()) {
+
+        for (int i = 0; i < mActivePointers.size(); i++) {
+            int key = mActivePointers.keyAt(i);
             PointF pointF = mActivePointers.get(key);
-            if(pointF!=null){
-                mPaint.setColor(COLORS[key%7]);
-                canvas.drawCircle(pointF.x,pointF.y,RADIUS,mPaint);
+            if (pointF != null) {
+                mPaint.setColor(COLORS[key % 7]);
+                canvas.drawCircle(pointF.x, pointF.y, RADIUS, mPaint);
             }
         }
 
